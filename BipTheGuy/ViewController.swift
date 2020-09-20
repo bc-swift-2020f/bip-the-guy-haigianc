41113f462ad18287a595d2f9709cf6c3adbcc29a
Claudine Haigian <haigianc@bc.edu>
2020-09-20 14:08:49 -0400

    Initial Commit

---
+++ BipTheGuy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BipTheGuy
+//
+//  Created by Claudine Haigian on 9/20/20.
+//  Copyright © 2020 Claudine Haigian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
